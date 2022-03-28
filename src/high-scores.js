6a80d2bb4dc49c7ce54556de58bcf57afc5fc1c8
Matilda Ako <matildaako@gmail.com>
2022-03-28 23:32:47 +0100

    Level 2 complete

--- src/high-scores.js
+++ src/high-scores.js
@@ -1,30 +1,35 @@
 import React from "react";
 
 const HighScores = (props) => {
-  console.log(props.scores)
-  return <div><h1> HIGH SCORES PER COUNTRY</h1>
-{   (props.scores.map((score) => {
-    return (
-      <div>
-        <h2>{`HIGH SCORES: ${score.name}`}</h2>
-        <table>
-          <thead>
-            {score.scores.map((person, index) => {
-              // console.log(person)
-              return (
-                <tr key={index}>
-                  <td>
-                    {person.n}: {person.s}
-                  </td>
-                </tr>
-              );
-            })}
-          </thead>
-        </table>
-      </div>
-    );
-  }))}
+  console.log(props.scores);
+  return (
+    <div>
+      <h1> HIGH SCORES PER COUNTRY</h1>
+      {props.scores
+        .sort((a, b) => (a.name === b.name ? 0 : a.name > b.name ? 1 : -1))
+        .map((score) => {
+          return (
+            <div>
+              <h2>{`HIGH SCORES: ${score.name}`}</h2>
+              <table>
+                <thead>
+                  {score.scores.map((person, index) => {
+                    return (
+                      <tr key={index}>
+                        <td>
+                          {person.n}: {person.s}
+                        </td>
+                      </tr>
+                    );
+                  })}
+                </thead>
+              </table>
+            </div>
+          );
+        })}
+      {/* {console.log(props.scores.sort((a, b) => a.name === b.name ? 0 : a.name > b.name ? 1 : -1))} */}
     </div>
+  );
 }
 
 export default HighScores;

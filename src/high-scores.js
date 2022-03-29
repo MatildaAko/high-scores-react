785a56dcacc43b1801d04770271cb7f43c478f19
Matilda Ako <matildaako@gmail.com>
2022-03-29 12:53:56 +0100

    Level 5 complete

--- src/high-scores.js
+++ src/high-scores.js
@@ -6,11 +6,37 @@ const HighScores = (props) => {
     setSortOrder(!sortOrder);
   }
   let sorted;
-  // let sortButton = document.querySelector(".sortButton");
-// console.log(sortButton.innerHTML)
+  let people = [];
   return (
     <div>
-      <h1> HIGH SCORES PER COUNTRY</h1>
+      <h1>All HIGH SCORES</h1>
+      <table>
+        <thead>
+          <tr>
+            <td>Position</td>
+            <td>Person</td>
+            <td>Score</td>
+          </tr>
+        </thead>
+        <tbody>
+          {props.scores
+            .map((country) => {
+              country.scores.map((person) => {
+                return people.push(person);
+              });
+            })}
+          {people.sort((a, b) => (a.s === b.s ? 0 : a.s < b.s ? 1 : -1)).map((person, index) => {
+            return (
+              <tr key={index}>
+                <td>{index + 1}</td>
+                <td>{person.n}</td>
+                <td>{person.s}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <h1> HIGH SCORES BY COUNTRY</h1>
       <button className="sortButton" onClick={changeOrder}>
         Sort Scores
       </button>
@@ -19,26 +45,31 @@ const HighScores = (props) => {
         .map((score) => {
           if (sortOrder === true) {
             sorted = score.scores.sort((a, b) => (a.s === b.s ? 0 : a.s < b.s ? 1 : -1));
-            // sortButton.innerHTML = "Sort Descending";
           } else {
             sorted = score.scores.sort((a, b) => (a.s === b.s ? 0 : a.s > b.s ? 1 : -1));
-            // sortButton.innerHTML = "Sort Ascending";
           }
           return (
             <div>
               <h2>{`HIGH SCORES: ${score.name}`}</h2>
               <table>
                 <thead>
+                  <tr>
+                    <td>Position</td>
+                    <td>Person</td>
+                    <td>Score</td>
+                  </tr>
+                </thead>
+                <tbody>
                   {sorted.map((person, index) => {
                     return (
                       <tr key={index}>
-                        <td>
-                          {person.n}: {person.s}
-                        </td>
+                        <td>{index + 1}</td>
+                        <td>{person.n}</td>
+                        <td>{person.s}</td>
                       </tr>
                     );
                   })}
-                </thead>
+                </tbody>
               </table>
             </div>
           );

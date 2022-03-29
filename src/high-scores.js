387dc5844700845d0291eaf9e22ae83d48be4076
Matilda Ako <matildaako@gmail.com>
2022-03-29 11:59:23 +0100

    Level 4 complete

--- src/high-scores.js
+++ src/high-scores.js
@@ -1,37 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 
 const HighScores = (props) => {
-  console.log(props.scores);
+  const [sortOrder, setSortOrder] = useState(true);
+  function changeOrder() {
+    setSortOrder(!sortOrder);
+  }
+  let sorted;
+  // let sortButton = document.querySelector(".sortButton");
+// console.log(sortButton.innerHTML)
   return (
     <div>
       <h1> HIGH SCORES PER COUNTRY</h1>
+      <button className="sortButton" onClick={changeOrder}>
+        Sort Scores
+      </button>
       {props.scores
         .sort((a, b) => (a.name === b.name ? 0 : a.name > b.name ? 1 : -1))
         .map((score) => {
+          if (sortOrder === true) {
+            sorted = score.scores.sort((a, b) => (a.s === b.s ? 0 : a.s < b.s ? 1 : -1));
+            // sortButton.innerHTML = "Sort Descending";
+          } else {
+            sorted = score.scores.sort((a, b) => (a.s === b.s ? 0 : a.s > b.s ? 1 : -1));
+            // sortButton.innerHTML = "Sort Ascending";
+          }
           return (
             <div>
               <h2>{`HIGH SCORES: ${score.name}`}</h2>
               <table>
                 <thead>
-                  {score.scores
-                    .sort((a, b) => (a.s === b.s ? 0 : a.s < b.s ? 1 : -1))
-                    .map((person, index) => {
-                      return (
-                        <tr key={index}>
-                          <td>
-                            {person.n}: {person.s}
-                          </td>
-                        </tr>
-                      );
-                    })}
+                  {sorted.map((person, index) => {
+                    return (
+                      <tr key={index}>
+                        <td>
+                          {person.n}: {person.s}
+                        </td>
+                      </tr>
+                    );
+                  })}
                 </thead>
               </table>
             </div>
           );
         })}
-      {/* {console.log(props.scores.sort((a, b) => a.name === b.name ? 0 : a.name > b.name ? 1 : -1))} */}
     </div>
   );
-}
+};
 
 export default HighScores;
